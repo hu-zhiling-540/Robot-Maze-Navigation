be996c4ad6089114654aad0c89f369643a1af090
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-04-25 01:36:32 -0400

    A behavior class for reaching the goal

---
+++ src/ReachGoal.java
@@ -0,0 +1,64 @@
+import lejos.nxt.Sound;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.subsumption.Behavior;
+
+public class ReachGoal implements Behavior{
+
+	public DifferentialPilot robot;
+	protected boolean isGoal;
+	
+	private static final short[] note = {2349,115, 0,5, 1760,165, 0,35};
+	
+	
+	// http://www.lejos.org/nxt/nxj/tutorial/LCD_Sensors/LCD_Sensors.htm#5
+	public void playSound() throws InterruptedException	{
+		for(int i=0;i <note.length; i+=2) {
+			short w = note[i+1];
+			int n = note[i];
+			if (n != 0) {
+				Sound.playTone(n, w*10);
+			}
+			Thread.sleep(w*10);
+		}
+	}
+	
+	/**
+	 * constructor
+	 * @param robot
+	 */
+	public ReachGoal(DifferentialPilot robot)	{
+		this.robot = robot;
+		isGoal = true;
+	}
+	
+	
+	@Override
+	public boolean takeControl() {
+		return isGoal;
+	}
+
+	@Override
+	public void action() {
+		for(int i=0;i <note.length; i+=2) {
+			short w = note[i+1];
+			int n = note[i];
+			if (n != 0) {
+				Sound.playTone(n, w*10);
+			}
+			try {
+				Thread.sleep(w*10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	@Override
+	public void suppress() {
+		robot.stop();	
+		
+	}
+
+}

98c49c091204f8aec92c56bb3e61c2032967558b
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-04-26 22:22:59 -0400

    Initialize cell value with number of paths remaining

--- src/World.java
+++ src/World.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class World {
 	
@@ -5,27 +6,68 @@ public class World {
 	public Cell start;		// start location
 	public Cell goal;		// goal location
 	
+	public ArrayList<Cell> stack;
+	public ArrayList<Cell> path;
+	
+	private int numRows;
+	private int numCols;;
+	
 //	private static final cell_width = 
 
-	public World(int numRows, int numCols, Cell start, Cell goal)	{
-		this.start = start;
-		this.goal = goal;
-		this.world = new Cell[numRows][numCols];
+	public World(int numRows, int numCols, int[] start, int[] goal)	{
+		
+		this.numRows = numRows;
+		this.numCols = numCols;
+		
+		world = new Cell[numRows][numCols];
 		populateWorld();
+		
+		this.start = world[start[0]][start[1]];
+		this.goal = world[goal[0]][goal[1]];
+		
+		stack = new ArrayList<Cell>();
+		path = new ArrayList<Cell>();
 	}
+	
 	
 	/**
 	 * Fills the world grid with cells
 	 */
 	public void populateWorld() {
-		for(int i = 0; i < world.length; i++) {
-			for(int j = 0; j < world[i].length; j++)	{
+		for(int i = 0; i < numRows; i++) {
+			for(int j = 0; j < numCols; j++)	{
 				world[i][j] = new Cell(i,j);
-				// g(n) at first
-				int dis = manhattanDist(world[i][j]);
-				world[i][j].setManhattanDist(dis);
 			}
 		}
+		
+		// corner cells
+	   	world[0][0].setValue(2);					// (0,0)
+	   	world[0][numCols-1].setValue(2);			// (0,7)
+	   	world[numRows-1][0].setValue(2);			// (4,0)
+	   	world[numRows-1][numCols-1].setValue(2);	// (4,7)
+	   	
+		// inner cells
+//	   	For i = 1 to 3
+//	   			For j = 1 to 6
+	   	for(int i = 1; i < numRows - 1; i++)	{
+    		for(int j = 1; j < numCols - 1; j++)	{
+    			world[i][j].setValue(4);
+    		}
+    	}
+	   	
+	   	// other border cells
+//	   	i = 0/4, loop j = 1 to 6
+//	   			j = 0/7, loop i = 1 to 3
+	   	for (int j = 1; j < numCols - 1; j++)	{
+	   		world[0][j].setValue(3);
+	   		world[numRows-1][j].setValue(3);
+	   	}
+	   	for (int i = 1; i < numRows -1; i++)	{
+	   		world[i][0].setValue(3);
+	   		world[i][numCols-1].setValue(3);
+	   	}
+	   	
+	   	
 	}
 	
 	
@@ -54,18 +96,18 @@ public class World {
 	}
 	
 	
-	/**
-	 * Calculates the Manhattan Distance bewteen goal and a certain cell
-	 * @param cell
-	 * @return
-	 */
-	public int manhattanDist(Cell cell)	{
-		int row = cell.pos[0];
-		int col = cell.pos[1];
-		int absD = Math.abs(row - goal.pos[0]) + Math.abs(col - goal.pos[1]);
-		return absD;
-	}
-	
+//	/**
+//	 * Calculates the Manhattan Distance bewteen goal and a certain cell
+//	 * @param cell
+//	 * @return
+//	 */
+//	public int manhattanDist(Cell cell)	{
+//		int row = cell.pos[0];
+//		int col = cell.pos[1];
+//		int absD = Math.abs(row - goal.pos[0]) + Math.abs(col - goal.pos[1]);
+//		return absD;
+//	}
+//	
 	
 	/**
 	 * Prints the world and the value each cell holds
@@ -80,6 +122,8 @@ public class World {
 		}
 		System.out.println(s);
 	}
+	
+	
 	
 	
 }

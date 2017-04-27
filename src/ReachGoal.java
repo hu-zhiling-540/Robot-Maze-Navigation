4981eac260db201e1d2830c4bee272a2c5bb7196
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-04-26 22:22:34 -0400

    If reach goal, play the music and generate the path back to the start.

--- src/ReachGoal.java
+++ src/ReachGoal.java
@@ -2,14 +2,27 @@ import lejos.nxt.Sound;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.subsumption.Behavior;
 
+
+/**
+ * The goal class implements Behavior, 
+ * plays a short sample music when reaching the goal,
+ * generates the final path ,
+ * and travels back to the starting cell
+ *
+ */
 public class ReachGoal implements Behavior{
 
 	public DifferentialPilot robot;
 	protected boolean isGoal;
-	
 	private static final short[] note = {2349,115, 0,5, 1760,165, 0,35};
 	
 	
+	public ReachGoal(DifferentialPilot robot, Boolean reachGoal)		{
+		this.robot = robot;
+		isGoal = reachGoal;
+		System.out.println("Goal");
+		
+	}
 	// http://www.lejos.org/nxt/nxj/tutorial/LCD_Sensors/LCD_Sensors.htm#5
 	public void playSound() throws InterruptedException	{
 		for(int i=0;i <note.length; i+=2) {

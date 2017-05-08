cd46819d361a45e3b18ae465a3a4304d2000dbf6
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-05-08 03:50:06 -0400

    helper methods
    markObstacle(), checkAdjacent(), updateCurrCell, orientation,
    isValidMove
    commandForExplore() will be directly called by Explore

--- src/World.java
+++ src/World.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 /**
- * World that holds the 2D array
+ * maze that holds the 2D array
  * and information on start and goal cells
  *  contains a lot of getter methods which return variables for the other classes
 in the application to use,
@@ -9,15 +9,25 @@ in the application to use,
  */
 public class World {
 	
-	public Cell[][] world;	//world grid
-	public Cell start;		// start location
-	public Cell goal;		// goal location
+	public Cell[][] maze;	//maze grid
+//	public Cell start;		// start location
+//	public Cell goal;		// goal location
 	
-	public ArrayList<Cell> stack;
+	public int[] start;		// start location
+	public int[] goal;		// goal location
+	
+	public ArrayList<Cell> newPath;
 	public ArrayList<Cell> path;
 	
 	private int numRows;
 	private int numCols;;
+	
+	private boolean goalReached = false;
+	
+	private int curr_row;
+	private int curr_col;
+	
+	private static final double cellD = 23;
 	
 
 	/**
@@ -33,60 +43,75 @@ public class World {
 		this.numCols = numCols;
 		
 		// generates a numRows * numCols grid
-		world = new Cell[numRows][numCols];
-		populateWorld();
+		maze = new Cell[numRows][numCols];
+		populatemaze();
 		
-		this.start = world[start[0]][start[1]];		// set up starting cell's coordinates
-		this.goal = world[goal[0]][goal[1]];		// set up goal cell's coordinates
+		curr_row = start[0];
+		curr_col = start[1];
+		
+		path = new ArrayList<Cell>();
+		path.add(getCurrCell());		// (1,1)
+		
+//		this.start = maze[start[0]][start[1]];		// set up starting cell's coordinates
+//		this.goal = maze[goal[0]][goal[1]];		// set up goal cell's coordinates
 	}
 	
+	/**
+	 * Returns the current cell
+	 * @return
+	 */
+	public Cell getCurrCell()	{
+		return maze[curr_row][curr_col];
+	}
+
 	
 	/**
-	 * Populates the world grid with cells
+	 * Populates the grid with cells,
+	 * and marks cell value respectively
 	 */
-	public void populateWorld() {
-		// populate the world with Cells
+	public void populatemaze() {
+		
+		// populate the maze with Cells
 		for(int i = 0; i < numRows; i++) {
 			for(int j = 0; j < numCols; j++)	{
-				world[i][j] = new Cell(i,j);
+				maze[i][j] = new Cell(i,j);
 			}
 		}
 		
 		// walls
 		for (int i = 0; i < numRows; i++)	{
-			world[i][0].setValue(-1);
-			world[i][numCols-1].setValue(-1);
+			maze[i][0].setObstacle();
+			maze[i][numCols-1].setObstacle();
 		}
 		for (int j = 0; j < numCols; j++)	{
-			world[0][j].setValue(-1);
-			world[numRows-1][j].setValue(-1);
+			maze[0][j].setObstacle();
+			maze[numRows-1][j].setObstacle();
 			
 		}
 		
 		// corner cells
-	   	world[1][1].setValue(2);					// (1,1)
-	   	world[1][numCols-2].setValue(2);			// (1,8)
-	   	world[numRows-2][1].setValue(2);			// (5,1)
-	   	world[numRows-2][numCols-2].setValue(2);	// (5,8)
+	   	maze[1][1].setValue(2);					// (1,1)
+	   	maze[1][numCols-2].setValue(2);			// (1,8)
+	   	maze[numRows-2][1].setValue(2);			// (5,1)
+	   	maze[numRows-2][numCols-2].setValue(2);	// (5,8)
 	   	
 	   	// other border cells
 		for (int i = 2; i < numRows-2; i++)	{
-	   		world[i][1].setValue(3);
-	   		world[i][numCols-2].setValue(3);
+	   		maze[i][1].setValue(3);
+	   		maze[i][numCols-2].setValue(3);
 	   	}
+		
 	   	for (int j = 2; j < numCols-2; j++)	{
-	   		world[1][j].setValue(3);
-	   		world[numRows-2][j].setValue(3);
+	   		maze[1][j].setValue(3);
+	   		maze[numRows-2][j].setValue(3);
 	   	}
 	   	
 		// inner cells
 	   	for(int i = 2; i < numRows-2; i++)	{
     		for(int j = 2; j < numCols - 2; j++)	{
-    			world[i][j].setValue(4);
+    			maze[i][j].setValue(4);
     		}
     	}
-	   	
-//	   	printWorld(world);	// print out the world for checking
 	}
 	
 	
@@ -95,11 +120,11 @@ public class World {
 	 */
 	public void createAPath()	{
 		
-		stack = new ArrayList<Cell>();
+		newPath = new ArrayList<Cell>();
 		path = new ArrayList<Cell>();
 		
-		path.add(start);		// adds starting cell as the first step
-		stack.add(0, start);	// adds starting cell
+//		path.add(start);		// adds starting cell as the first step
+//		newPath.add(0, start);	// adds starting cell
 		dfs();
 	}
 	
@@ -121,7 +146,7 @@ public class World {
 	 * @param cell
 	 */
 	public void setVisited(Cell cell)	{
-		world[cell.row][cell.col].setVisited();
+		maze[cell.row][cell.col].setVisited();
 	}
 	
 	
@@ -130,7 +155,7 @@ public class World {
 	 * @param cell
 	 */
 	public void setObstacle(Cell cell)	{
-		world[cell.row][cell.col].setObstacle();;
+		maze[cell.row][cell.col].setObstacle();;
 	}
 	
 	
@@ -151,25 +176,25 @@ public class World {
 			ArrayList<Cell> nbrs = new ArrayList<Cell>();
 			
 			// adjacent top cell
-			if (world[temp.row+1][temp.col].visited)
-				nbrs.add(world[temp.row+1][temp.col]);
+			if (maze[temp.row+1][temp.col].visited)
+				nbrs.add(maze[temp.row+1][temp.col]);
 			
 			// adjacent bottom cell
-			if (world[temp.row-1][temp.col].visited)
-				nbrs.add(world[temp.row-1][temp.col]);
+			if (maze[temp.row-1][temp.col].visited)
+				nbrs.add(maze[temp.row-1][temp.col]);
 			
 			// adjacent left cell
-			if (world[temp.row][temp.col-1].visited)
-				nbrs.add(world[temp.row][temp.col-1]);
+			if (maze[temp.row][temp.col-1].visited)
+				nbrs.add(maze[temp.row][temp.col-1]);
 			
 			// adjacent right cell
-			if (world[temp.row][temp.col+1].visited)	
-				nbrs.add(world[temp.row][temp.col+1]);
+			if (maze[temp.row][temp.col+1].visited)	
+				nbrs.add(maze[temp.row][temp.col+1]);
 			
 			if (nbrs.size()>0)	{
 				Cell next = nbrs.get(0);
 				path.add(next);
-				stack.add(0, next);
+				nbrs.add(0, next);
 			}
 
 			dfs();		// recursive call
@@ -184,10 +209,178 @@ public class World {
 	 * @return
 	 */
 	public Boolean isVisited(int col, int row)	{
-		if (world[row][col].visited)
+		if (maze[row][col].visited)
 			return true;
 		
 		return false;
+	}
+
+	/**
+	 * Marks the current cell as an obstacle
+	 */
+	public void markObstacle() {
+		maze[curr_row][curr_col].setObstacle();
+	}
+
+	/**
+	 * 
+	 * It will check through adjacent neighbors to find all legal states 
+	 * succeeding the current cell
+	 */
+	public ArrayList<Cell> checkAdjacent()	{
+		
+		ArrayList<Cell> nbrs = new ArrayList<Cell>();	// a list of neighbors
+		int counter = 0;	// keeps track of number of emerging routes
+			
+		// up
+		if (isValidMove(curr_row+1, curr_col)){
+//		if (!maze[curr_row+1][curr_col].isObstacle() && !maze[curr_row+1][curr_col].isDeadEnd() && !maze[curr_row+1][curr_col].visited)	{
+			nbrs.add(0,maze[curr_row+1][curr_col]);
+			counter ++;
+			System.out.println("check up");
+		}
+		// down
+		if (isValidMove(curr_row-1, curr_col)){
+//		if (!maze[curr_row-1][curr_col].isObstacle() && !maze[curr_row-1][curr_col].isDeadEnd() && !maze[curr_row-1][curr_col].visited)	{
+			nbrs.add(0,maze[curr_row-1][curr_col]);
+			System.out.println("check down");
+			counter ++;
+		}
+		// left
+		if (isValidMove(curr_row, curr_col-1)){
+//		if (!maze[curr_row][curr_col-1].isObstacle() && !maze[curr_row][curr_col-1].isDeadEnd() && !maze[curr_row][curr_col-1].visited)	{
+			nbrs.add(0,maze[curr_row][curr_col-1]);
+			System.out.println("check left");
+			counter ++;
+		}
+		// right
+		if (isValidMove(curr_row, curr_col+1)){
+//		if (!maze[curr_row][curr_col+1].isObstacle() && !maze[curr_row][curr_col+1].isDeadEnd() && !maze[curr_row][curr_col+1].visited)	{
+			nbrs.add(0,maze[curr_row][curr_col+1]);
+			System.out.println("check right");
+			counter ++;	
+		}
+		
+		// no possible routes emerging from it
+		if(counter == 0)
+			getCurrCell().setDeadEnd();	// visited and no routes left
+		
+		return nbrs;
+	 }
+	
+	public Cell nextCell()	{
+		/* can be transformed to global */
+		ArrayList<Cell> possibleMoves = checkAdjacent();
+		Cell next = null;
+		// if there is at least one route left
+		if (!getCurrCell().isDeadEnd() || !possibleMoves.isEmpty())
+			next = possibleMoves.remove(0);	// pop out the first cell
+		
+		// reach to a dead end, should backtrack
+		else	{
+			// set back to previous cell
+			updateCurrCell(getPrev());
+			// next will be null for this case
+		}
+		
+		return next;
+		
+	}
+	
+	public Cell getPrev()	{
+		return path.get(path.size()-1);
+	}
+	
+	/**
+	 * A method that will be called by Explore behavior
+	 * to find a possible move and orientation for that move if exists
+	 * @return
+	 */
+	public int commandForExplore()	{
+		
+		int command = -1;	// nothing found
+		
+		Cell next = nextCell();
+		
+		if (next != null)	{
+			command = orientation(next);
+			path.add(next);		// takes in next cell
+			updateCurrCell(next);
+		}
+		
+		// next == null; dead end
+		else
+			command = 1;	// backtrack to prev cell
+		
+		return command;
+			
+	}
+	
+
+	/**
+	 * Resets the current cell to the cell passed in
+	 * by updating the coordinates
+	 * @param cell
+	 */
+	public void updateCurrCell(Cell cell)	{
+		curr_row = cell.row;
+		curr_col = cell.col;
+	}
+	
+	public int orientation(Cell next)	{
+		/* NEEDS TO BE DISCUSSED */
+		Cell temp = next;
+		Cell prev = getPrev();
+		
+		// if prev and curr are in the same row
+		if (prev.row == getCurrCell().row)		{
+			
+			// temp is in the lower left corner
+			if ((temp.row < prev.row && temp.col < prev.col) 
+					||(temp.row > prev.row && temp.col > prev.col))	{	// or upper right corner
+				return -90;		// left
+			}
+			
+			// if prev, curr, temp are not in the same row, we can go right
+			else if (getCurrCell().row != temp.row)
+				return 90;		// right
+			
+			// if prev, curr, temp are in the same row, no need to rotate
+			else
+				return 0;
+		}
+		
+		// if prev and curr are in the same col
+		else		{
+			// temp is in the upper right corner
+			if ((temp.row > prev.row && temp.col > prev.col) 
+					||(temp.row < prev.row && temp.col < prev.col))		{	// or lower left corner
+				return 90;		// right
+			}
+			
+			// if prev, curr, temp are not in the same col, we can go left
+			else if (getCurrCell().col != temp.col)	{
+				return -90;		// left
+			}
+			// if prev, curr, temp are in the same col, no need to rotate
+			else
+				return 0;
+		}
+			
+	}
+	
+	/**
+	 * Identifies whether a cell is a legal move
+	 * by checking the cell value
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	public boolean isValidMove(int row, int col) {
+		// cell value is -1 or =1
+		if (maze[row][col].isObstacle() || maze[row][col].isDeadEnd())
+			return false;
+		return true;
 	}
 	
 	

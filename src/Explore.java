cfddb36553b24f2dc3a811c2021ca15430e71682
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-05-08 03:48:08 -0400

    ask the world to help find next cell and navigate

--- src/Explore.java
+++ src/Explore.java
@@ -14,7 +14,7 @@ public class Explore implements Behavior {
 
 	public DifferentialPilot robot;
 	public World world;
-	public Cell[][] maze;
+//	public Cell[][] maze;
 
 	public Cell prev;
 	public Cell curr;
@@ -36,9 +36,9 @@ public class Explore implements Behavior {
 		this.robot = robot;
 		this.world = world;		// the world class 
 		
-		maze = world.world;		// the grid
-		
-		prev = world.start;
+//		maze = world.world;		// the grid
+//		
+//		prev = world.start;
 		curr = prev;
 		
 	}
@@ -57,6 +57,8 @@ public class Explore implements Behavior {
 		
 		suppressed = false;		// set the flag to false
 		
+		int c = world.commandForExplore();
+		commandDecode(c);
 		try {
 			Thread.yield();
 			Thread.sleep(1000); // stops for a short time (one second)
@@ -64,155 +66,56 @@ public class Explore implements Behavior {
 		catch(InterruptedException ie) {}
 		
 		
-		/* First, Check current cell */
-		
-		// if it is a starting cell
-		if (curr.row == world.start.row && curr.col == world.start.col)
-			world.setVisited(curr);
-		
-		// if it is identified as an obstacle (cell value = -1)
-		if (curr.isObstacle())		{
-			toCheck.remove(curr);
-			prev.removeAPath();		// a path has been found as a dead end
-			curr = prev;	// backtracking
-		}
-		
-		// for next step
-		// explore adjacent cells 
-		checkAround(curr);
-		
-		// after checking adjacent cells, nothing in the stack
-		if (toCheck.isEmpty() && curr.isDeadEnd())
-			return;		// exit
-		
-		Cell temp = toCheck.remove(0);		// next step to be taken
-		
-		// have the chance to walk back to the previous cell
-		if (temp.row == prev.row && temp.col == prev.col)
-			prev = curr;	// override it to be the current cell
-		
-		// other cells
-		else	{
-			
-			// if prev and curr are in the same row
-			if (prev.row == curr.row)		{
-				
-				// temp is in the lower left corner
-				if ((temp.row < prev.row && temp.col < prev.col) 
-						||(temp.row > prev.row && temp.col > prev.col))	{	// or upper right corner
-					robot.rotateLeft();
-					try {
-						Thread.yield();
-						Thread.sleep(1000); // stops for a short time (one second)
-					}
-					
-					catch(InterruptedException ie) {}
-				}
-				
-				// if prev, curr, temp are not in the same row, we can go right
-				else if (curr.row != temp.row)	{
-					robot.rotateRight();
-					try {
-						Thread.yield();
-						Thread.sleep(1000); // stops for a short time (one second)
-					}
-					
-					catch(InterruptedException ie) {}
-				}
-				
-				// if prev, curr, temp are in the same row, no need to rotate
-				
-				// travel in the designated direction
-				robot.travel(cellD);
-				try {
-					Thread.yield();
-					Thread.sleep(1000); // stops for a short time (one second)
-				}
-				
-				catch(InterruptedException ie) {}
-			}
-			
-			// if prev and curr are in the same col
-			else		{
-				// temp is in the upper right corner
-				if ((temp.row > prev.row && temp.col > prev.col) 
-						||(temp.row < prev.row && temp.col < prev.col))		{	// or lower left corner
-					robot.rotateRight();
-					try {
-						Thread.yield();
-						Thread.sleep(1000); // stops for a short time (one second)
-					}
-					
-					catch(InterruptedException ie) {}
-				}
-				
-				// if prev, curr, temp are not in the same col, we can go left
-				else if (curr.col != temp.col)	{
-					robot.rotateLeft();
-					try {
-						Thread.yield();
-						Thread.sleep(1000); // stops for a short time (one second)
-					}
-					
-					catch(InterruptedException ie) {}
-				}
-				// if prev, curr, temp are in the same col, no need to rotate
-				
-				// travel in the designated direction
-				robot.travel(cellD);
-				try {
-					Thread.yield();
-					Thread.sleep(1000); // stops for a short time (one second)
-				}
-				
-				catch(InterruptedException ie) {}
-				
-			}
-		}
-		
-		world.setVisited(curr);		// sync with the maze in the world
-		prev = curr;
-		curr = temp;
-		
 	}
 	
-	
-	/**
-	 * It will check through adjacent neighbors to find all legal states 
-	 * succeeding the current cell
-	 * @param cell
-	 */
-	public void checkAround(Cell cell)	{
-		int counter = 0;
+	public void commandDecode(int c)	{
+		// current cell is a dead end, backtrack
+		if (c == 1)
+			robot.travel(-cellD);	//move backward for one cell distance
 		
-		// up
-		if (!maze[cell.row+1][cell.col].isObstacle() && !maze[cell.row+1][cell.col].isDeadEnd() && !maze[cell.row+1][cell.col].visited)	{
-			toCheck.add(0,maze[cell.row+1][cell.col]);
-			counter ++;
-			System.out.println("check up");
-		}
-		// down
-		if (!maze[cell.row-1][cell.col].isObstacle() && !maze[cell.row-1][cell.col].isDeadEnd() && !maze[cell.row-1][cell.col].visited)	{
-			toCheck.add(0,maze[cell.row-1][cell.col]);
-			System.out.println("check down");
-			counter ++;
-		}
-		// left
-		if (!maze[cell.row][cell.col-1].isObstacle() && !maze[cell.row][cell.col-1].isDeadEnd() && !maze[cell.row][cell.col-1].visited)	{
-			toCheck.add(0,maze[cell.row][cell.col-1]);
-			System.out.println("check left");
-			counter ++;
-		}
-		// right
-		if (!maze[cell.row][cell.col+1].isObstacle() && !maze[cell.row][cell.col+1].isDeadEnd() && !maze[cell.row][cell.col+1].visited)	{
-			toCheck.add(0,maze[cell.row][cell.col+1]);
-			System.out.println("check right");
-			counter ++;	
+		// left turn
+		else if ( c == -90)		{
+			robot.rotateLeft();
+			try {
+				Thread.yield();
+				Thread.sleep(1000); // stops for a short time (one second)
+			}
+			catch(InterruptedException ie) {}
+			
+			robot.travel(cellD);
+			try {
+				Thread.yield();
+				Thread.sleep(1000); // stops for a short time (one second)
+			}
+			catch(InterruptedException ie) {}
 		}
 		
-		// if no possible routes emerging from it
-		if(counter == 0)
-			cell.setDeadEnd();
+		// right turn
+		else if ( c == 90)	{
+			robot.rotateRight();
+			try {
+				Thread.yield();
+				Thread.sleep(1000); // stops for a short time (one second)
+			}
+			catch(InterruptedException ie) {}
+			
+			robot.travel(cellD);
+			try {
+				Thread.yield();
+				Thread.sleep(1000); // stops for a short time (one second)
+			}
+			catch(InterruptedException ie) {}
+		}
+		
+		// same direction, no need to rotate
+		else	{
+			robot.travel(cellD);
+			try {
+				Thread.yield();
+				Thread.sleep(1000); // stops for a short time (one second)
+			}
+			catch(InterruptedException ie) {}
+		}	
 	}
 	
 	/*
